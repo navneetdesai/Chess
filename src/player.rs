6942b1c551e2673ba7cea0d22fa8386c16c998a0
Navneet Desai <navneetdesai44@gmail.com>
2022-05-06 12:58:24 -0400

    Add raw code for player.rs

---
+++ src/player.rs
@@ -0,0 +1,23 @@
+use crate::piece::{Color, Piece};
+
+pub struct Player {
+    name: String,
+    color: Color,
+}
+
+impl Player {
+    pub fn new(name: String, color: Color) -> Self {
+        Player {
+            name,
+            color,
+        }
+    }
+
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn get_color(&self) -> &Color {
+        &self.color
+    }
+}
