ffad0d95db1b11fbe6ca3eebfc198071bab18f94
Navneet Desai <navneetdesai44@gmail.com>
2022-05-06 13:25:58 -0400

    Add main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
+mod board;
+mod chess;
+mod piece;
+mod player;
+mod error;
+mod square;
+use crate::chess::Chess;
+use colored::*;
+use std::io::{stdout, Write};
 
+fn main() {
+    let (name1, name2) = crate::get_player_names();
+    let mut game = Chess::new(String::from(name1), String::from(name2));
+    game.start();
+}
+
+/// Gets the player names
+pub fn get_player_names() -> (String, String) {
+    print!("Please enter name for player 1(white): ");
+    let mut name1 = String::new();
+    stdout().flush().unwrap();
+    std::io::stdin()
+        .read_line(&mut name1)
+        .expect("Umm, system crashed. Please restart.");
+    print!("\nPlease enter name for player 2(black): ");
+    let mut name2 = String::new();
+    stdout().flush().unwrap();
+    std::io::stdin()
+        .read_line(&mut name2)
+        .expect("Umm, system crashed. Please restart.");
+    (String::from(name1.trim()), String::from(name2.trim()))
 }
